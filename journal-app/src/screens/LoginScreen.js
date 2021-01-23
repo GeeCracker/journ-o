5f9a434bee2b41456b6de5612cf78a5271e54f4b
Graham Carkner <gcarkner@gmail.com>
2021-01-22 21:24:02 -0500

    implemented router

--- journal-app/src/screens/LoginScreen.js
+++ journal-app/src/screens/LoginScreen.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 class Login extends React.Component {
     render() {
-      return <h1>login screen</h1>;
+      return (
+        <>
+
+        <Link to="/profile">clickme</Link>
+
+        <h1>login screen</h1>
+
+        </>
+      );
     }
   }
 
