6062f5276462ab246df5aabc62b2e341906c3c42
Graham Carkner <gcarkner@gmail.com>
2021-01-23 11:20:36 -0500

    topbar

---
+++ journal-app/src/components/Topbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './styles.css';
+
+import { Link } from 'react-router-dom';
+
+class Topbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    signOut = () => {
+        alert("sign out button");
+    }
+    
+    render() {
+        return (
+            <div class="topbar">
+                <div class="topbar-title">Journ-o</div>
+                <div class="outline-button">sign out</div>
+            </div>
+        );
+    }
+}
+
+export default Topbar;
