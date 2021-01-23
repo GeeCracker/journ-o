5ace96a7e3b46f9475364b266ee1cccf3032bd31
Graham Carkner <gcarkner@gmail.com>
2021-01-22 22:54:43 -0500

    hotfix

--- journal-app/src/index.js
+++ journal-app/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Firebase, { FirebaseContext } from './components/Firebase';
+import Firebase, { FirebaseContext } from './components/firebase';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
   <FirebaseContext.Provider value={new Firebase()}>
     <App />
   </FirebaseContext.Provider>,
