a9de2fda40ee23c751516351c147c91fb2e1df90
Graham Carkner <gcarkner@gmail.com>
2021-01-22 22:49:48 -0500

    signup form implementation

--- journal-app/src/screens/LoginScreen.js
+++ journal-app/src/screens/LoginScreen.js
@@ -1,19 +1,45 @@
 import React from 'react';
 
 import LoginForm from '../components/LoginForm';
+import SignupForm from '../components/SignupForm';
 
 import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            signup: false
+        };
+    }
+
+    toLogin = () => {
+        this.setState({signup: false});
+    }
+
+    toSignup = () => {
+        this.setState({signup: true});
+    }
+
     render() {
       return (
         <>
 
         <Link to="/profile">clickme</Link>
 
-        <h1>login screen</h1>
+        {this.state.signup ? 
+            <>
+                <h1>signup screen</h1> 
+                <SignupForm />
+            </>:
+            <>
+                <h1>login screen</h1>
+                <LoginForm />
+            </>
+        }
 
-        <LoginForm />
+        <button onClick={this.toLogin}>login</button>
+        <button onClick={this.toSignup}>signup</button>
 
         </>
       );
