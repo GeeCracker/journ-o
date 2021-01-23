35e2579f24b24e64a121e4003db3aede768104ac
Graham Carkner <gcarkner@gmail.com>
2021-01-23 16:16:32 -0500

    im a certified hood gangster

---
+++ journal-app/src/components/QuestionCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import './styles.css';
+
+import FillButton from './FillButton';
+
+class QuestionCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    onButtonClick = () => {
+        alert("swag");
+    }
+    
+    render() {
+        return (
+            <div class="qcard">
+                <div class="qcard-top">
+                    <div class="box">{this.props.question.question}</div>
+                </div>
+                <div class="qcard-bottom">
+                    <div class="box">
+                        {this.props.question.choices.map((item, index) => (
+                            <FillButton 
+                                text={item.answer} // answer option text
+                                active={this.props.choice == index} // choice.picked (1 or 0)
+                                index={index} // index of choice
+                                handleClick={this.props.handleClick}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default QuestionCard;
--- journal-app/src/screens/NewJournalScreen.js
+++ journal-app/src/screens/NewJournalScreen.js
@@ -1,8 +1,60 @@
 import React from 'react';
 
+import QuestionCard from '../components/QuestionCard';
+import Topbar from '../components/Topbar';
+
 class NewJournal extends React.Component {
-    render() {
-      return <h1>new journal screen</h1>;
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [
+        {
+          id: 1,
+          type: 'mc',
+          question: 'How much milk did you drink today?',
+          choices: [
+            {
+              answer: 'so much.',
+              targetid: 2,
+              picked: 0
+            },
+            {
+              answer: 'I suck a cow tiddy.',
+              targetid: 3,
+              picked: 0
+            },
+            {
+              answer: 'I\'m lactose swactose my guy.',
+              targetid: 4,
+              picked: 0
+            }
+          ]
+        }
+      ],
+      currentQuestion: 0,
+      currentChoice: 0,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(index){
+    this.setState({currentChoice: index});
+  }
+
+  render() {
+      return (
+        <div style={{position:'absolute',backgroundColor: '#19456B',height:'100vh',width:'100vw'}}>
+
+          <Topbar />
+          <QuestionCard  
+            question={this.state.questions[this.state.currentQuestion]}
+            choice={this.state.currentChoice}
+            handleClick={this.handleClick}
+            />
+        
+        </div>
+      );
     }
   }
 
