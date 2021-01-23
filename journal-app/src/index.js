f628480e2182301627a880b74cc5248b00b2679a
erarbuthnot <elliot.arbuthnot@gmail.com>
2021-01-22 22:13:21 -0500

    firebase setup

--- journal-app/src/index.js
+++ journal-app/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
